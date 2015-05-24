926f41a16c9970dd70cd07c3e875ce1f9b98e02b
dijdev <danieljonesdev@gmail.com>
2015-05-24 00:28:10 -0400

    Signin & Signup views modified. App structure learning. Bossable MEAN Tut Vid 9

--- www/public/modules/core/controllers/home.client.controller.js
+++ www/public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,44 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		$scope.alerts = [
+			{
+				icon: 'glyphicon-user',
+				color: 'btn-success',
+				total: '20,408',
+				description: 'TOTAL CUSTOMERS'
+			},
+			{
+				icon: 'glyphicon-calendar',
+				color: 'btn-primary',
+				total: '5345',
+				description: 'Upcoming Events'
+			},
+			{
+				icon: 'glyphicon-edit',
+				color: 'btn-success',
+				total: '117',
+				description: 'NEWEST CUSTOMERS'
+			},
+			{
+				icon: 'glyphicon-record',
+				color: 'btn-info',
+				total: '9999999',
+				description: 'EMAILS SENT'
+			},
+			{
+				icon: 'glyphicon-eye-open',
+				color: 'btn-warning',
+				total: '20',
+				description: 'FOLLOW UPS REQUIRED'
+			},
+			{
+				icon: 'glyphicon-flag',
+				color: 'btn-danger',
+				total: '23',
+				description: 'TO-DOS'
+			},
+		];
 	}
 ]);
